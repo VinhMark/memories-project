c39c9dba8907b92fc202500b9ece6408e89c961c
= <=>
2022-11-29 13:28:29 +0700

    init project

---
+++ server/models/postMessage.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+
+const postSChema = mongoose.Schema({
+  title: String,
+  message: String,
+  creator: String,
+  tags: [String],
+  selectedFile: String,
+  likeCount: {
+    type: Number,
+    default: 0,
+  },
+  createAt: {
+    type: Date,
+    default: new Date(),
+  },
+});
+
+const PostMessage = mongoose.model('PostMessage', postSChema);
+export default PostMessage;
